fc343474a48384abdc203369d6b3893d69971f3d
Rob Winch <rwinch@users.noreply.github.com>
2019-02-06 21:56:41 -0600

    Push Does Not Create Backport If Fix Refers Directly to Milestone
    If the Fixes: gh-<number> refers to an issue that is the milestone for
    the branch being pushed to. An issue is no longer created.

--- src/main/kotlin/io/spring/github/event/DefaultGithubEventService.kt
+++ src/main/kotlin/io/spring/github/event/DefaultGithubEventService.kt
@@ -50,10 +50,13 @@ class DefaultGithubEventService(val backport : BackportService) : GithubEventSer
     private fun backport(branch: BranchRef, issueNumber : Int) : Mono<Boolean> {
         val issue = IssueRef(branch.repository, issueNumber)
         return backport.findMilestoneNumber(branch)
-            .filterWhen { milestoneNumber -> backport.isIssueForMilestone(issue, milestoneNumber).map { isIssue -> !isIssue } }
+            .filterWhen { milestoneNumber -> isNotIssueForMilestone(issue, milestoneNumber) }
             .flatMap { milestoneNumber -> backport.createBackport(issue, milestoneNumber, listOf()).then(Mono.just(true)) }
             .defaultIfEmpty(false)
     }
+
+    private fun isNotIssueForMilestone(issue: IssueRef, milestoneNumber: Int) =
+            backport.isIssueForMilestone(issue, milestoneNumber).map { isIssue -> !isIssue }
 
     override fun backport(pushEvent : PushEvent) : Mono<Boolean> {
         val githubRef = pushEvent.getBranchRef()
@@ -69,10 +72,19 @@ class DefaultGithubEventService(val backport : BackportService) : GithubEventSer
 
     private fun backport(fixedCommits: List<PushEvent.Commit>, githubRef: BranchRef, milestoneNumber: Int, pushEvent: PushEvent): Mono<Boolean> {
         return Flux.fromIterable(fixedCommits)
+                .filterWhen { fixedCommit ->
+                    val issueRef = IssueRef(githubRef.repository, fixedCommit.getFixIssueId()!!)
+                    // FIXME: We should refine this
+                    //  We are looking up the backportedIssueForMilestone twice perhaps
+                    //  need to move combined logic into private method here and only have
+                    //  way to determine this specific issue (not any linked issues) have
+                    //  this milestone
+                    isNotIssueForMilestone(issueRef, milestoneNumber)
+                }
                 .flatMap { fixedCommit ->
                     backport(githubRef, fixedCommit, milestoneNumber, pushEvent)
                 }
-                .then(Mono.just(true))
+                .any { r -> r }
     }
 
     private fun backport(githubRef: BranchRef, fixedCommit: PushEvent.Commit, milestoneNumber: Int, pushEvent: PushEvent): Mono<Boolean> {

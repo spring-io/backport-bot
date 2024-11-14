6c1d06bd4653e0f4ae6eae246247b613ccf03ffd
Artem Bilan <artem.bilan@broadcom.com>
2024-11-14 15:25:03 -0500

    Add `Fixes|Closes` matching for issue link, too
    The short syntax like `Fixes: #123` is nice,
    but we lose a link to the target issue when we deal with commit history
    outside the GitHub.
    For example, I use commits UI in the IntelliJ IDEA and would like
    to jump to the issue by clicking link from the commit message.
    Adding an issue link into a `Fixes|Closes` pattern would let me avoid
    adding extra link into commit message and this bot would still do its job.
    
    Also add a `junit-vintage-engine` dependency to be able to run JUnit 4 test
    from Gradle

--- src/main/kotlin/io/spring/github/event/PushEvent.kt
+++ src/main/kotlin/io/spring/github/event/PushEvent.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ class PushEvent(val ref : String, val repository : Repository, val pusher : Push
 
 
     data class Commit(val id : String, val message : String) {
-        val r = """.*?(Fixes|Closes):?\s+(gh\-|#)(?<id>\d+)(\r?\n)*""".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.DOT_MATCHES_ALL))
+        val r = """.*?(Fixes|Closes):?\s+(gh-|#|https://github.com/.*/.*/issues/)(?<id>\d+)(\r?\n)*""".toRegex(setOf(RegexOption.IGNORE_CASE, RegexOption.DOT_MATCHES_ALL))
 
         fun getFixIssueId() : Int? {
             return r.find(message)?.groups?.get("id")?.value?.toInt()
